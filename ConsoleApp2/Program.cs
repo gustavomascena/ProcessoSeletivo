351ee7c76a9a1d3a1945c125383a03c5acbfe51c
Metrodata <Metrodata@METRODATA_ELE>
2022-06-17 17:02:01 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ultimoNumero = 1;
+            int penultimoNumero = 0;
+            int soma = ultimoNumero + penultimoNumero;
+
+            Console.WriteLine("Digite um número inteiro.");
+            int numeroDigitado = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(penultimoNumero);
+            Console.WriteLine(ultimoNumero);
+
+            while (soma < numeroDigitado)
+            {
+                penultimoNumero = ultimoNumero;
+                ultimoNumero = soma;
+                soma = ultimoNumero + penultimoNumero;
+                Console.WriteLine(soma);
+            }
+            if (soma == numeroDigitado || ultimoNumero == numeroDigitado || penultimoNumero == numeroDigitado)
+            {
+                Console.WriteLine("O número que você digitou pertence a sequência de Fibonacci!");
+            }
+            else
+            {
+                Console.WriteLine("O número que você digitou não pertence a sequência de Fibonacci!");
+            }
+        }
+    }
+}
+
